267b557c847a917d57c2b1f47d039e6cd88e2678
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-02-18 18:46:00 +0530

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,3 +10,6 @@ class LoginPage(BasePage):
         self.fill(self.USERNAME, username)
         self.fill(self.PASSWORD, password)
         self.click(self.LOGIN_BTN)
+
+    def get_error_message(self):
+        return self.get_text(self.ERROR_MSG)
